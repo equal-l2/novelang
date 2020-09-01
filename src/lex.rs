51366bc2e8b1fae6a6ae4f67003db5589da74dc9
equal-l2 <eng.equall2@gmail.com>
2020-09-02 02:49:01 +0900

    Finish lex

--- src/lex.rs
+++ src/lex.rs
@@ -1,5 +1,33 @@
+trait ToItem
+where
+    Self: Sized + Copy + 'static,
+{
+    const DISCRIMINANTS: &'static [Self];
+
+    fn as_str(&self) -> &str;
+
+    fn check(s: &[char]) -> Option<Self> {
+        'outer: for res in Self::DISCRIMINANTS {
+            let res_chars: Vec<_> = res.as_str().chars().collect();
+            if res_chars.len() <= s.len() {
+                for i in 0..res_chars.len() {
+                    if res_chars[i].to_lowercase().ne(s[i].to_lowercase()) {
+                        continue 'outer;
+                    }
+                }
+                return Some(*res);
+            }
+        }
+        None
+    }
+
+    fn len(&self) -> usize {
+        self.as_str().len()
+    }
+}
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum ResKind {
+enum Reserved {
     // keywords
     AsMut,
     Be,
@@ -26,25 +54,33 @@ enum ResKind {
     DisableWait,
 }
 
-impl ResKind {
-    fn check_reserved(s: &[char]) -> Option<ResKind> {
-        'outer: for res in Self::iter() {
-            let res_chars: Vec<_> = res.as_str().chars().collect();
-            if res_chars.len() <= s.len() {
-                for i in 0..res_chars.len() {
-                    if res_chars[i].to_lowercase().ne(s[i].to_lowercase()) {
-                        continue 'outer;
-                    }
-                }
-                return Some(*res);
-            }
-        }
-        None
-    }
-
-    fn len(&self) -> usize {
-        self.as_str().len()
-    }
+impl ToItem for Reserved {
+    const DISCRIMINANTS: &'static [Self] = &[
+        // keywords
+        Self::AsMut,
+        Self::Be,
+        Self::To,
+        Self::Dice,
+        Self::With,
+        Self::Face,
+        // insts
+        Self::Print,
+        Self::Sub,
+        Self::Call,
+        Self::While,
+        Self::Let,
+        Self::Modify,
+        Self::Input,
+        Self::If,
+        Self::ElIf,
+        Self::Else,
+        Self::End,
+        Self::Roll,
+        Self::Halt,
+        Self::Break,
+        Self::EnableWait,
+        Self::DisableWait,
+    ];
 
     fn as_str(&self) -> &str {
         match self {
@@ -75,47 +111,96 @@ impl ResKind {
         }
     }
 
-    fn iter() -> impl Iterator<Item = &'static Self> {
-        [
-            // keywords
-            ResKind::AsMut,
-            ResKind::Be,
-            ResKind::To,
-            ResKind::Dice,
-            ResKind::With,
-            ResKind::Face,
-            // insts
-            ResKind::Print,
-            ResKind::Sub,
-            ResKind::Call,
-            ResKind::While,
-            ResKind::Let,
-            ResKind::Modify,
-            ResKind::Input,
-            ResKind::If,
-            ResKind::ElIf,
-            ResKind::Else,
-            ResKind::End,
-            ResKind::Roll,
-            ResKind::Halt,
-            ResKind::Break,
-            ResKind::EnableWait,
-            ResKind::DisableWait,
-        ].into_iter()
+    fn check(s: &[char]) -> Option<Self> {
+        'outer: for res in Self::DISCRIMINANTS {
+            let res_chars: Vec<_> = res.as_str().chars().collect();
+            if res_chars.len() <= s.len() {
+                for i in 0..res_chars.len() {
+                    if res_chars[i].to_lowercase().ne(s[i].to_lowercase()) {
+                        continue 'outer;
+                    }
+                }
+                // For Reserved we need this check to separate Ident
+                // (example: "be" is Reserved but "bed" is Ident)
+                if res_chars.len() == s.len() || is_sep(s[res_chars.len()]) {
+                    return Some(*res);
+                }
+            }
+        }
+        None
     }
 }
 
-#[derive(Debug, Clone)]
-enum OpKind {}
+#[derive(Debug, Clone, Copy)]
+enum AriOps {
+    Add, // +
+    Sub, // -
+    Mul, // *
+    Div, // /
+    Mod, // %
+}
+
+impl ToItem for AriOps {
+    const DISCRIMINANTS: &'static [Self] = &[
+        Self::Add, // +
+        Self::Sub, // -
+        Self::Mul, // *
+        Self::Div, // /
+        Self::Mod, // %
+    ];
+    fn as_str(&self) -> &str {
+        match self {
+            Self::Add => "+",
+            Self::Sub => "-",
+            Self::Mul => "*",
+            Self::Div => "/",
+            Self::Mod => "%",
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+enum RelOps {
+    Equal,        // ==
+    NotEqual,     // !=
+    LessEqual,    // <=
+    GreaterEqual, // >=
+    LessThan,     // <
+    GreaterThan,  // >
+}
+
+impl ToItem for RelOps {
+    const DISCRIMINANTS: &'static [Self] = &[
+        Self::Equal,        // ==
+        Self::NotEqual,     // !=
+        Self::LessEqual,    // <=
+        Self::GreaterEqual, // >=
+        Self::LessThan,     // <
+        Self::GreaterThan,  // >
+    ];
+    fn as_str(&self) -> &str {
+        match self {
+            Self::Equal => "==",
+            Self::NotEqual => "!=",
+            Self::LessEqual => "<=",
+            Self::GreaterEqual => ">=",
+            Self::LessThan => "<",
+            Self::GreaterThan => ">",
+        }
+    }
+}
 
 type NumType = i64;
 #[derive(Debug, Clone)]
-enum TokenKind {
-    Res(ResKind),
-    Op(OpKind),
+enum Item {
+    Res(Reserved),
+    Ari(AriOps),
+    Rel(RelOps),
     Num(NumType),
     Ident(String),
+    Str(String),
     Semi,
+    Comma,
 }
 
 #[derive(Debug, Clone)]
@@ -127,12 +212,16 @@ pub struct Location {
 #[derive(Clone)]
 pub struct Token {
     pub loc: Location,
-    kind: TokenKind,
+    item: Item,
 }
 
 impl std::fmt::Debug for Token {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "Token {{ {:?} ({},{}) }}", self.kind, self.loc.row, self.loc.col)
+        write!(
+            f,
+            "Token {{ {:?} ({},{}) }}",
+            self.item, self.loc.row, self.loc.col
+        )
     }
 }
 
@@ -143,70 +232,105 @@ pub struct Lexed {
 }
 
 #[derive(Debug, Clone)]
-pub struct Error {}
+pub enum Error {
+    UnterminatedStr,
+}
 
-fn is_qual(c: char) -> bool {
-    !c.is_whitespace() && c != ';'
+const RESERVED_CHARS: &'static [char] =
+    &['+', '-', '*', '/', '%', '"', '<', '>', '!', '=', ';', ','];
+
+fn is_ident_char(c: char) -> bool {
+    !c.is_whitespace() && !RESERVED_CHARS.contains(&c)
+}
+
+fn is_sep(c: char) -> bool {
+    c.is_whitespace() || c == ';'
 }
 
 pub fn lex(s: String) -> Result<Lexed, Error> {
     let mut tks = Vec::new();
     let lines: Vec<_> = s.lines().map(String::from).collect();
     for (idx, l) in lines.iter().enumerate() {
+        eprintln!("line {}", idx);
         let v: Vec<_> = l.chars().collect();
         let mut i = 0;
         while i < v.len() {
+            let loc = Location {
+                row: idx + 1,
+                col: i + 1,
+            };
+            if v[i] == '#' {
+                break;
+            }
             if v[i].is_whitespace() {
                 i += 1;
-            }
-            else if v[i] == ';' {
+            } else if v[i] == ';' {
                 tks.push(Token {
-                    loc: Location {
-                        row: idx + 1,
-                        col: i + 1,
-                    },
-                    kind: TokenKind::Semi,
+                    loc,
+                    item: Item::Semi,
                 });
                 i += 1;
-            } else if let Some(res) = ResKind::check_reserved(&v[i..l.len()]) {
-                let res_len = res.len();
-                if l.len() == res_len || v[i + res_len] == ';' || v[i + res_len].is_whitespace() {
-                    tks.push(Token {
-                        loc: Location {
-                            row: idx + 1,
-                            col: i + 1,
-                        },
-                        kind: TokenKind::Res(res),
-                    });
-                    i += res_len;
+            } else if v[i] == ',' {
+                tks.push(Token {
+                    loc,
+                    item: Item::Comma,
+                });
+                i += 1;
+            } else if v[i] == '"' {
+                i += 1;
+                let mut s = String::new();
+                while i < v.len() {
+                    if v[i] == '"' {
+                        break;
+                    }
+                    s.push(v[i]);
+                    i += 1;
                 }
+                if v[i] != '"' {
+                    return Err(Error::UnterminatedStr);
+                }
+                tks.push(Token {
+                    loc,
+                    item: Item::Str(s),
+                });
+                i += 1;
+            } else if let Some(res) = Reserved::check(&v[i..v.len()]) {
+                tks.push(Token {
+                    loc,
+                    item: Item::Res(res),
+                });
+                i += res.len();
+            } else if let Some(res) = AriOps::check(&v[i..v.len()]) {
+                tks.push(Token {
+                    loc,
+                    item: Item::Ari(res),
+                });
+                i += res.len();
+            } else if let Some(res) = RelOps::check(&v[i..v.len()]) {
+                tks.push(Token {
+                    loc,
+                    item: Item::Rel(res),
+                });
+                i += res.len();
             } else if v[i].is_numeric() {
-                let orig_i = i;
                 let mut s = String::new();
                 while i < v.len() && v[i].is_numeric() {
                     s.push(v[i]);
                     i += 1;
                 }
                 tks.push(Token {
-                    loc: Location {
-                        row: idx + 1,
-                        col: orig_i + 1,
-                    },
-                    kind: TokenKind::Num(s.parse().unwrap()),
+                    loc,
+                    item: Item::Num(s.parse().unwrap()),
                 });
             } else {
-                let orig_i = i;
                 let mut s = String::new();
-                while i < v.len() && is_qual(v[i]) {
+                while i < v.len() && is_ident_char(v[i]) {
                     s.push(v[i]);
                     i += 1;
                 }
                 tks.push(Token {
-                    loc: Location {
-                        row: idx + 1,
-                        col: orig_i + 1,
-                    },
-                    kind: TokenKind::Ident(s),
+                    loc,
+                    item: Item::Ident(s),
                 });
             }
         }
